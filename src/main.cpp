6a3b68fe04f35d110985fb9b5a74387ce0bda192
Jeremy Runyan <GoogleBot42@users.noreply.github.com>
2019-10-20 18:14:48 -0400

    Combine vertex buffers into one

--- src/main.cpp
+++ src/main.cpp
@@ -12,81 +12,43 @@
 #include "camera.h"
 
 float cube_vertices[] = {
-    -1.0f,-1.0f,-1.0f,
-    -1.0f,-1.0f, 1.0f,
-    -1.0f, 1.0f, 1.0f,
-    1.0f, 1.0f,-1.0f,
-    -1.0f,-1.0f,-1.0f,
-    -1.0f, 1.0f,-1.0f,
-    1.0f,-1.0f, 1.0f,
-    -1.0f,-1.0f,-1.0f,
-    1.0f,-1.0f,-1.0f,
-    1.0f, 1.0f,-1.0f,
-    1.0f,-1.0f,-1.0f,
-    -1.0f,-1.0f,-1.0f,
-    -1.0f,-1.0f,-1.0f,
-    -1.0f, 1.0f, 1.0f,
-    -1.0f, 1.0f,-1.0f,
-    1.0f,-1.0f, 1.0f,
-    -1.0f,-1.0f, 1.0f,
-    -1.0f,-1.0f,-1.0f,
-    -1.0f, 1.0f, 1.0f,
-    -1.0f,-1.0f, 1.0f,
-    1.0f,-1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f,-1.0f,-1.0f,
-    1.0f, 1.0f,-1.0f,
-    1.0f,-1.0f,-1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f,-1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f, 1.0f,-1.0f,
-    -1.0f, 1.0f,-1.0f,
-    1.0f, 1.0f, 1.0f,
-    -1.0f, 1.0f,-1.0f,
-    -1.0f, 1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    -1.0f, 1.0f, 1.0f,
-    1.0f,-1.0f, 1.0f
-};
-
-float cube_colors[] = {
-    0.583f,  0.771f,  0.014f,
-    0.609f,  0.115f,  0.436f,
-    0.327f,  0.483f,  0.844f,
-    0.822f,  0.569f,  0.201f,
-    0.435f,  0.602f,  0.223f,
-    0.310f,  0.747f,  0.185f,
-    0.597f,  0.770f,  0.761f,
-    0.559f,  0.436f,  0.730f,
-    0.359f,  0.583f,  0.152f,
-    0.483f,  0.596f,  0.789f,
-    0.559f,  0.861f,  0.639f,
-    0.195f,  0.548f,  0.859f,
-    0.014f,  0.184f,  0.576f,
-    0.771f,  0.328f,  0.970f,
-    0.406f,  0.615f,  0.116f,
-    0.676f,  0.977f,  0.133f,
-    0.971f,  0.572f,  0.833f,
-    0.140f,  0.616f,  0.489f,
-    0.997f,  0.513f,  0.064f,
-    0.945f,  0.719f,  0.592f,
-    0.543f,  0.021f,  0.978f,
-    0.279f,  0.317f,  0.505f,
-    0.167f,  0.620f,  0.077f,
-    0.347f,  0.857f,  0.137f,
-    0.055f,  0.953f,  0.042f,
-    0.714f,  0.505f,  0.345f,
-    0.783f,  0.290f,  0.734f,
-    0.722f,  0.645f,  0.174f,
-    0.302f,  0.455f,  0.848f,
-    0.225f,  0.587f,  0.040f,
-    0.517f,  0.713f,  0.338f,
-    0.053f,  0.959f,  0.120f,
-    0.393f,  0.621f,  0.362f,
-    0.673f,  0.211f,  0.457f,
-    0.820f,  0.883f,  0.371f,
-    0.982f,  0.099f,  0.879f
+    // Position, Color
+    -1.0f,  -1.0f,  -1.0f,      0.583f,  0.771f,  0.014f,
+    -1.0f,  -1.0f,   1.0f,      0.609f,  0.115f,  0.436f,
+    -1.0f,   1.0f,   1.0f,      0.327f,  0.483f,  0.844f,
+     1.0f,   1.0f,  -1.0f,      0.822f,  0.569f,  0.201f,
+    -1.0f,  -1.0f,  -1.0f,      0.435f,  0.602f,  0.223f,
+    -1.0f,   1.0f,  -1.0f,      0.310f,  0.747f,  0.185f,
+     1.0f,  -1.0f,   1.0f,      0.597f,  0.770f,  0.761f,
+    -1.0f,  -1.0f,  -1.0f,      0.559f,  0.436f,  0.730f,
+     1.0f,  -1.0f,  -1.0f,      0.359f,  0.583f,  0.152f,
+     1.0f,   1.0f,  -1.0f,      0.483f,  0.596f,  0.789f,
+     1.0f,  -1.0f,  -1.0f,      0.559f,  0.861f,  0.639f,
+    -1.0f,  -1.0f,  -1.0f,      0.195f,  0.548f,  0.859f,
+    -1.0f,  -1.0f,  -1.0f,      0.014f,  0.184f,  0.576f,
+    -1.0f,   1.0f,   1.0f,      0.771f,  0.328f,  0.970f,
+    -1.0f,   1.0f,  -1.0f,      0.406f,  0.615f,  0.116f,
+     1.0f,  -1.0f,   1.0f,      0.676f,  0.977f,  0.133f,
+    -1.0f,  -1.0f,   1.0f,      0.971f,  0.572f,  0.833f,
+    -1.0f,  -1.0f,  -1.0f,      0.140f,  0.616f,  0.489f,
+    -1.0f,   1.0f,   1.0f,      0.997f,  0.513f,  0.064f,
+    -1.0f,  -1.0f,   1.0f,      0.945f,  0.719f,  0.592f,
+     1.0f,  -1.0f,   1.0f,      0.543f,  0.021f,  0.978f,
+     1.0f,   1.0f,   1.0f,      0.279f,  0.317f,  0.505f,
+     1.0f,  -1.0f,  -1.0f,      0.167f,  0.620f,  0.077f,
+     1.0f,   1.0f,  -1.0f,      0.347f,  0.857f,  0.137f,
+     1.0f,  -1.0f,  -1.0f,      0.055f,  0.953f,  0.042f,
+     1.0f,   1.0f,   1.0f,      0.714f,  0.505f,  0.345f,
+     1.0f,  -1.0f,   1.0f,      0.783f,  0.290f,  0.734f,
+     1.0f,   1.0f,   1.0f,      0.722f,  0.645f,  0.174f,
+     1.0f,   1.0f,  -1.0f,      0.302f,  0.455f,  0.848f,
+    -1.0f,   1.0f,  -1.0f,      0.225f,  0.587f,  0.040f,
+     1.0f,   1.0f,   1.0f,      0.517f,  0.713f,  0.338f,
+    -1.0f,   1.0f,  -1.0f,      0.053f,  0.959f,  0.120f,
+    -1.0f,   1.0f,   1.0f,      0.393f,  0.621f,  0.362f,
+     1.0f,   1.0f,   1.0f,      0.673f,  0.211f,  0.457f,
+    -1.0f,   1.0f,   1.0f,      0.820f,  0.883f,  0.371f,
+     1.0f,  -1.0f,   1.0f,      0.982f,  0.099f,  0.879f
 };
 
 class cubeapp : public Application {
@@ -95,7 +57,6 @@ public:
     ~cubeapp() override
     {
         delete cubeVbo;
-        delete colorVbo;
         delete program;
         delete vao;
         delete texture;
@@ -117,8 +78,6 @@ private:
         // create VBOs
         cubeVbo = new VBO;
         cubeVbo->setData(sizeof(cube_vertices), cube_vertices);
-        colorVbo = new VBO;
-        colorVbo->setData(sizeof(cube_colors), cube_colors);
 
         // shader
         program = new GLSLProgram;
@@ -133,9 +92,12 @@ private:
         // create VAO
         vao = new VAO;
         cubeVbo->bind();
-        vao->setAttributePointer(VAOAttribute(program->getAttributeLocation("position"), GL_FLOAT, 3));
-        colorVbo->bind();
-        vao->setAttributePointer(VAOAttribute(program->getAttributeLocation("color"), GL_FLOAT, 3));
+        VAOAttribute attributes[] = {
+            VAOAttribute(program->getAttributeLocation("position"), GL_FLOAT, 3),
+            VAOAttribute(program->getAttributeLocation("color"), GL_FLOAT, 3),
+            VAOAttribute::END,
+        };
+        vao->setAttributePointers(attributes);
         vao->bind();
     }
 
